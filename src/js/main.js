15ba6b839e67e1732897bca5665bb186c4115277
Borys <borys.baca@gmail.com>
2022-05-17 15:11:52 +0200

    js readability corrected

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,12 @@
 if (!localStorage.counter || (isNaN(localStorage.counter))) {
     localStorage.counter = 0;
 }
-
 const closeBtn = document.querySelector('#close-btn');
 const moduleBtn = document.querySelector('#module-btn');
-
 // in the case there are many of them on the same page
 // const moduleBtns = document.querySelectorAll('.module__button');
-
 const modal = document.querySelector('#modal');
 const resetBtn = document.querySelector('#modal-reset-btn');
-
-
 // FUNCTIONS SECTION DOWN HERE 
 const modalClosing = () => {
     modal.classList.remove('active');
@@ -23,22 +18,17 @@ const alert = (counter) => {
 const clickIncrease = () => {
     localStorage.counter++;
     const modalCounter = document.querySelector('#modal-counter');
-
-
     if (localStorage.counter >= 5) {
         localStorage.counter = 5;
         alert(modalCounter);
     }
-
 }
 const resetCounting = () => {
     localStorage.counter = 0;
-    clicksCounter = 0;
     modalClosing();
     console.log('You can now click again ! Enjoy yourself :)');
 }
 // LISTENERS DOWN HERE 
-
 closeBtn.addEventListener('click', modalClosing);
 moduleBtn.addEventListener('click', clickIncrease);
 
@@ -46,7 +36,6 @@ moduleBtn.addEventListener('click', clickIncrease);
 // moduleBtns.forEach((btn) => {
 //     btn.addEventListener('click', clickIncrease)
 // })
-
 resetBtn.addEventListener('click', resetCounting);
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
