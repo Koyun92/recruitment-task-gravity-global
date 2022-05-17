a30a3f9e3258ca3edcc6af7714fccd59bc86f4a7
Borys <borys.baca@gmail.com>
2022-05-17 11:34:53 +0200

    local storage funcionality added

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,50 @@
 const closeBtn = document.querySelector('#close-btn');
-// const moduleBtn = document.querySelector('#module-btn')
+const moduleBtn = document.querySelector('#module-btn');
+let clicksCounter = localStorage.counter;
 // in the case there are many of them on the same page
-const moduleBtns = document.querySelectorAll('.module__button');
-
+// const moduleBtns = document.querySelectorAll('.module__button');
 const modal = document.querySelector('#modal');
 const resetBtn = document.querySelector('#modal-reset-btn');
-let clicksCounter = 0;
+
+
 // FUNCTIONS SECTION DOWN HERE 
 const modalClosing = () => {
     modal.classList.remove('active');
 }
 const alert = (counter) => {
     modal.classList.add('active');
-    counter.innerHTML = `${clicksCounter} times`;
+    counter.innerHTML = `${localStorage.counter} times`;
 }
 const clickIncrease = () => {
+
+    clicksCounter++
+    localStorage.counter = clicksCounter;
     const modalCounter = document.querySelector('#modal-counter');
-    clicksCounter++;
-    if (clicksCounter === 5) {
-        alert(modalCounter);
-    } else if (clicksCounter > 5) {
+
+
+    if (localStorage.counter >= 5) {
         clicksCounter = 5;
+        localStorage.counter = 5;
         alert(modalCounter);
     }
+
 }
 const resetCounting = () => {
+    localStorage.counter = 0;
     clicksCounter = 0;
     modalClosing();
-    console.log('You can now click again till you fall asleep! Enjoy yourself :)');
+    console.log('You can now click again ! Enjoy yourself :)');
 }
 // LISTENERS DOWN HERE 
 
 closeBtn.addEventListener('click', modalClosing);
-// moduleBtn.addEventListener('click', clickIncrease);
+moduleBtn.addEventListener('click', clickIncrease);
 
 // in the case there are many of them on the same page
-moduleBtns.forEach((btn) => {
-    btn.addEventListener('click', clickIncrease)
-})
+// moduleBtns.forEach((btn) => {
+//     btn.addEventListener('click', clickIncrease)
+// })
+
 resetBtn.addEventListener('click', resetCounting);
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
