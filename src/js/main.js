c0d92c64da0002d5642292c718b450aecd86a581
Borys <borys.baca@gmail.com>
2022-05-17 09:15:04 +0200

    funcionality for multiply buttons  added

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,18 @@
 const closeBtn = document.querySelector('#close-btn');
-const moduleBtn = document.querySelector('#module-btn')
+// const moduleBtn = document.querySelector('#module-btn')
+// in the case there are many of them on the same page
+const moduleBtns = document.querySelectorAll('.module__button');
+
 const modal = document.querySelector('#modal');
-const resetBtn = document.querySelector('#modal-reset-btn')
+const resetBtn = document.querySelector('#modal-reset-btn');
 let clicksCounter = 0;
-
-
-
-
 // FUNCTIONS SECTION DOWN HERE 
-
 const modalClosing = () => {
     modal.classList.remove('active');
 }
-const alert = (modalCounter) => {
+const alert = (counter) => {
     modal.classList.add('active');
-    modalCounter.innerHTML = `${clicksCounter} times`;
+    counter.innerHTML = `${clicksCounter} times`;
 }
 const clickIncrease = () => {
     const modalCounter = document.querySelector('#modal-counter');
@@ -26,20 +24,21 @@ const clickIncrease = () => {
         alert(modalCounter);
     }
 }
-const reseCounting = () => {
+const resetCounting = () => {
     clicksCounter = 0;
     modalClosing();
     console.log('You can now click again till you fall asleep! Enjoy yourself :)');
 }
-
-
-
-
 // LISTENERS DOWN HERE 
 
 closeBtn.addEventListener('click', modalClosing);
-moduleBtn.addEventListener('click', clickIncrease);
-resetBtn.addEventListener('click', reseCounting);
+// moduleBtn.addEventListener('click', clickIncrease);
+
+// in the case there are many of them on the same page
+moduleBtns.forEach((btn) => {
+    btn.addEventListener('click', clickIncrease)
+})
+resetBtn.addEventListener('click', resetCounting);
 window.addEventListener('click', (event) => {
     if (event.target === modal) {
         modalClosing();
